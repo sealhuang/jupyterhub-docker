a40f674eaf4fd6904de308bbff72dbb1b1a53734
Lijie Huang <huanglijie@outlook.com>
2019-10-26 22:26:22 +0800

    update

--- jupyterhub/jupyterhub_config.py
+++ jupyterhub/jupyterhub_config.py
@@ -188,7 +188,9 @@ class BBAuthAuthenticator(OAuthenticator):
 
 ## Generic
 c.JupyterHub.admin_access = True
+# have JupyterLab start by default, or Jupyter Notebook
 c.Spawner.default_url = '/lab'
+#c.Spawner.default_url = '/tree/home/{username}'
 
 c.JupyterHub.authenticator_class = BBAuthAuthenticator
 
